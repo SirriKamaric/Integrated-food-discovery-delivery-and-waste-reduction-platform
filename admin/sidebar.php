7269a2736e5a268f1753aadd56d7d02aad33edd1
Yebit Christian Babila <christianbabilayebit@gmail.com>
2025-06-26 15:26:32 +0100

    Add admin/sidebar.php to Foodsave-yebit_christian

---
+++ admin/sidebar.php
@@ -0,0 +1,44 @@
+
+<div class="sidebar p-3">
+    <div class="text-center mb-4">
+        <h4>FoodSave</h4>
+        <p class="text-muted small">Admin Panel</p>
+    </div>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="dashboard.php">
+                <i class="fas fa-tachometer-alt"></i> Dashboard
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="users.php">
+                <i class="fas fa-users"></i> Manage Users
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="restaurants.php">
+                <i class="fas fa-store"></i> Restaurants
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="ngos.php">
+                <i class="fas fa-hands-helping"></i> NGOs
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="donations.php">
+                <i class="fas fa-donate"></i> Donations
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="reports.php">
+                <i class="fas fa-chart-bar"></i> Reports
+            </a>
+        </li>
+        <li class="nav-item mt-3">
+            <a class="nav-link" href="../logout.php">
+                <i class="fas fa-sign-out-alt"></i> Logout
+            </a>
+        </li>
+    </ul>
+</div>
